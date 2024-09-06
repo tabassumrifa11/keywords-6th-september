d356cbaf2d5a3a9fb1b1e1b90b57e72911ec50a9
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-09-06 17:23:34 +0600

    ttr

---
+++ pass.py
@@ -0,0 +1,12 @@
+for x in range(51):
+    if x % 15 == 0:
+        print(f"twist {x}")
+        
+    elif x % 15 == 0:
+        pass   
+    
+    elif x % 5 == 0:
+        print(f"fizz {x}")
+        
+    elif x % 3 == 0:
+        print(f"buzz {x}")    
